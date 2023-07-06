c3ca0f6307aacdf244a61f794373443364c9b2c3
Cam0Cow <roie.roiegal@gmail.com>
2023-07-05 22:57:10 -0500

    Enable round robin for real this time

--- uCOS-III/Source/os_cfg.h
+++ uCOS-III/Source/os_cfg.h
@@ -49,7 +49,7 @@
 #define OS_CFG_PRIO_MAX                32u   /* Defines the maximum number of task priorities (see OS_PRIO data type) */
 
 #define OS_CFG_SCHED_LOCK_TIME_MEAS_EN  0u   /* Include code to measure scheduler lock time                           */
-#define OS_CFG_SCHED_ROUND_ROBIN_EN     0u   /* Include code for Round-Robin scheduling                               */
+#define OS_CFG_SCHED_ROUND_ROBIN_EN     1u   /* Include code for Round-Robin scheduling                               */
 #define OS_CFG_STK_SIZE_MIN            64u   /* Minimum allowable task stack size                                     */
 
 
